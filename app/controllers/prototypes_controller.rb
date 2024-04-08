196fcdcf8f63190d19e983eea93a2bab1cf1e959
mameharu2020 <soayamay@gmail.com>
2024-04-09 00:13:58 +0900

    first commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,6 @@
 class PrototypesController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
 
   def index
     @prototypes = Prototype.all
@@ -26,6 +28,7 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+    redirect_to root_path unless @prototype.user == current_user
   end
 
   def update
@@ -49,4 +52,7 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
+  def move_to_index
+    redirect_to root_path unless user_signed_in?
+  end
 end
